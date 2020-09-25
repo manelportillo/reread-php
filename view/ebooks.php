2d20675e431a5c0ae97f7f297627576759b07c97
manelportillo <100006748.joan23@fje.edu>
2020-09-25 19:22:25 +0200

    Cambios paginas .html por .php

--- view/ebooks.php
+++ view/ebooks.php
@@ -18,9 +18,9 @@
     <div class="row">
       <div class="column left">
         <div class="topnav">
-          <a href="../index.html">Re-Read</a>
-          <a href="libros.html">Libros</a>
-          <a href="ebooks.html">eBooks</a>
+          <a href="../index.php">Re-Read</a>
+          <a href="libros.php">Libros</a>
+          <a href="ebooks.php">eBooks</a>
         </div>
         <h3>Toda la actualidad en eBooks</h3>
         <!--eBooks con descripción-->
